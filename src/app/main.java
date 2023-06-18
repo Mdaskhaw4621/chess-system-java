3d7e91b0375a59cd92a43eebe5f51781ec9c281e
Mdaskhaw4621 <moises.marques@ext.hdi.com>
2023-06-18 16:59:31 -0300

    Iniciando as implementações

--- src/app/main.java
+++ src/app/main.java
@@ -1,9 +1,12 @@
 package app;
 
+import boardgame.Board;
+
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		Board board = new Board(8, 8);
 	}
 
 }
